520fd096ce5fd42fced90a397b9294340f08ce51
AnnneRose <50593284+AnnneRose@users.noreply.github.com>
2023-01-24 14:31:55 -0500

    Add files via upload

---
+++ skeleton/client/src/components/pages/UserPage.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import GoogleLogin from 'react-google-login';
+
+const CLIENT_ID = "439284619534-8dgp40a4po4afq4fp9id9b5862c5h0q9.apps.googleusercontent.com";
+
+const UserPage = () => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get('/api/user', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        setUser(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const handleGoogleLoginSuccess = (response) => {
+    localStorage.setItem('token', response.tokenId);
+    fetchUser();
+  };
+
+  const handleGoogleLoginFailure = (response) => {
+    console.error(response);
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={CLIENT_ID}
+        onSuccess={handleGoogleLoginSuccess}
+        onFailure={handleGoogleLoginFailure}
+      />
+
+      {user.name ? (
+        <div>
+          <h1>{user.name}</h1>
+          <img src={user.profilePicture} alt={`${user.name}'s profile picture`} />
+          <p>{user.bio}</p>
+        </div>
+      ) : (
+        <p>Log in to view your page.</p>
+      )}
+    </div>
+  );
+};
+
+export default UserPage;
